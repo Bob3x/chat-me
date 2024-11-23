e97adcf5a9b69aa2c4016d13ce50af578e2d62a3
Bob3x <bob3xx@protonmail.com>
2024-11-23 22:01:21 +0200

    Basic formatting configurations

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  tabWidth: 4,
+  useTabs: false,
+  semi: true,
+  printWidth: 100,
+};
