da97c16bfba5dea5fb52d389c3d27413946c8037
Bob3x <bob3xx@protonmail.com>
2024-11-26 17:57:27 +0200

    Updating component with proper firebase initialization and duplicates

--- App.js
+++ App.js
@@ -1,33 +1,25 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Start from "./components/Start";
 import Chat from "./components/Chat";
-import {
-    EXPO_PUBLIC_FIREBASE_API_KEY,
-    EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-    EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    EXPO_PUBLIC_FIREBASE_APP_ID
-} from "@env";
+
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-    console.log("Env vars loaded:", !!process.env.EXPO_PUBLIC_FIREBASE_API_KEY);
+    // console.log("Env vars loaded:", !!process.env.EXPO_PUBLIC_FIREBASE_API_KEY);
     const firebaseConfig = {
-        apiKey: EXPO_PUBLIC_FIREBASE_API_KEY,
-        authDomain: EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-        storageBucket: EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId: EXPO_PUBLIC_FIREBASE_APP_ID
+        apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
     };
     console.log("Firebase Config:", firebaseConfig);
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
+    // Initialize Firebase and checks for dublicates
+    const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
     // Initialize Cloud Firestore and get a reference to the service
     const db = getFirestore(app);
@@ -36,9 +28,7 @@ const App = () => {
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Start">
                 <Stack.Screen name="Start" component={Start} />
-                <Stack.Screen name="Chat">
-                    {(props) => <Chat db={db} {...props} />}
-                </Stack.Screen>
+                <Stack.Screen name="Chat">{(props) => <Chat db={db} {...props} />}</Stack.Screen>
             </Stack.Navigator>
         </NavigationContainer>
     );
