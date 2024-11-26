6c5e0457841cc4380fe73f682aca9d4f9eafe083
Bob3x <bob3xx@protonmail.com>
2024-11-26 23:03:15 +0200

    Update adding connection status and storage

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, disableNetwork, enableNetwork } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { useNetInfo } from "@react-native-community/netinfo";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Start from "./components/Start";
 import Chat from "./components/Chat";
+import { useEffect } from "react";
+import { Alert } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
@@ -23,12 +27,34 @@ const App = () => {
 
     // Initialize Cloud Firestore and get a reference to the service
     const db = getFirestore(app);
+    const storage = getStorage(app);
+
+    // offline - online status
+    const connectionStatus = useNetInfo();
+
+    useEffect(() => {
+        if (connectionStatus.isConnected === false) {
+            Alert.alert("Connection lost!");
+            disableNetwork(db);
+        } else if (connectionStatus.isConnected === true) {
+            enableNetwork(db);
+        }
+    }, [connectionStatus.isConnected, db]);
 
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Start">
                 <Stack.Screen name="Start" component={Start} />
-                <Stack.Screen name="Chat">{(props) => <Chat db={db} {...props} />}</Stack.Screen>
+                <Stack.Screen name="Chat">
+                    {(props) => (
+                        <Chat
+                            isConnected={connectionStatus.isConnected}
+                            db={db}
+                            storage={storage}
+                            {...props}
+                        />
+                    )}
+                </Stack.Screen>
             </Stack.Navigator>
         </NavigationContainer>
     );
